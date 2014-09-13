6f11db9056978ea412bd7aaf5e88eded02274ccb
kunal <kunal@kk.(none)>
2014-09-13 17:02:20 +0530

    some not working code

--- client/client.py
+++ client/client.py
@@ -1,8 +1,9 @@
 import asyncore, socket
+import argparse
 
 class PAASClient(asyncore.dispatcher):
 
-    def __init__(self, host, path):
+    def __init__(self, host, path, buff):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect( (host, 8080) )
@@ -17,16 +18,28 @@ class PAASClient(asyncore.dispatcher):
         self.close()
 
     def handle_read(self):
-	
+	#print "Reading"	
         print self.recv(8192)
+	exit()
 
     def writable(self):
+	#print "Writable.."
         return (len(self.buffer) > 0)
 
     def handle_write(self):
+	#print "writeing..."
         sent = self.send(self.buffer)
-        self.buffer = self.buffer[sent:]
+	self.buffer = self.buffer[sent:]
+	
 
 
-client = PAASClient('localhost', '/')
-asyncore.loop()
+parser = argparse.ArgumentParser(description='DocPAAS Client')
+parser.add_argument('repolist', metavar='-rl', help='list all enviornment image at server')
+parser.add_argument('setup', metavar='-s', help='Setup environment for specified repo ')
+#p = PASSCmdLine(parser)
+args = parser.parse_args()
+print args
+
+if args.repolist == "232":
+	client = PAASClient('localhost', '/',args.setup)
+	asyncore.loop()

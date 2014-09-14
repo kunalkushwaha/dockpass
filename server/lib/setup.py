919a2a4dea74cf0f679da14a35f99d04581b7cc2
kunalkushwaha <kunal.kushwaha@gmail.com>
2014-09-14 13:32:45 +0530

    code for setup

--- server/lib/setup.py
+++ server/lib/setup.py
@@ -9,10 +9,22 @@ def do_docker_setup(tag):
                   version='1.12',
                   timeout=10)
     
+    fo = open("./repolist/nodejs/Dockerfile", "r")   
     
-    msg = c.images()
-    msgstr = json.dumps(msg)
-    print msgstr
+    #build_responce = c.build(path='./repolist/nodejs/', tag=tag, quiet=False,
+    #                         fileobj=None, nocache=False,
+    #                         rm=False, stream=False, timeout=None,
+    #                         custom_context=False, encoding='tar')
+    
+    build_responce = c.create_container("ubuntu", command='/bin/bash', hostname=None, user=None,
+                                   detach=False, stdin_open=False, tty=True, mem_limit=0,
+                                   ports=None, environment=None, dns=None, volumes=None,
+                                   volumes_from=None, network_disabled=False, name=tag,
+                                   entrypoint=None, cpu_shares=None, working_dir=None,
+                                   memswap_limit=0)
+    
+    print build_responce
+    #print json.dumps(build_responce)
  
-    return msgstr
+    return json.dumps(build_responce)
     
